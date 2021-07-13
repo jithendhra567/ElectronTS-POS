7dc0cf5fbc3e5147bc98d23a7cce8809f535c684
Srinivas Varma <mssvarma06@gmail.com>
2021-07-13 22:00:27 +0530

    admin panel

--- src/renderer/src/app/app-routing.module.ts
+++ src/renderer/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ItemComponent } from "./component/items/item.component";
-import { TableComponent } from "./component/tables/table.component";
 
 const routes: Routes = [
-  { path: "tables", component: TableComponent },
-  { path: "items", component: ItemComponent }
-  //  { path: '404', component: NotfoundComponent },
-  //  { path: '**', redirectTo: '/404' }
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin-panel/admin-panel.module').then(m => m.AdminPanelModule)
+  }
 ];
 
 @NgModule({
--- src/renderer/src/app/app.module.ts
+++ src/renderer/src/app/app.module.ts
@@ -5,39 +5,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { TableComponent } from "./component/tables/table.component";
-import { ItemComponent } from "./component/items/item.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AddTableComponent } from "./component/tables/add-table/add-table.component";
-import { ViewTableComponent } from "./component/tables/view-table/view-table.component";
-import { DeleteTableComponent } from "./component/tables/delete-table/delete-table.component";
-import { EditTableComponent } from "./component/tables/edit-table/edit-table.component";
-import { EditItemComponent } from "./component/items/edit-item/edit-item.component";
-import { DeleteItemComponent } from "./component/items/delete-item/delete-item.component";
-import { AddItemComponent } from "./component/items/add-item/add-item.component";
-import { AddCategoryComponent } from "./component/items/add-category/add-category.component";
-import { EditCategoriesComponent } from "./component/items/edit-categories/edit-categories.component";
-import { ChangeTableComponent } from "./component/tables/change-table/change-table.component";
-import { AssignCategoryComponent } from "./component/items/assign-category/assign-category.component";
-import { AdminPanelComponent } from "./admin-panel/admin-panel.component";
+import { AdminPanelModule } from "./admin-panel/admin-panel.module";
 
 @NgModule({
   declarations: [
-    AdminPanelComponent,
-    AppComponent,
-    TableComponent,
-    AddTableComponent,
-    ViewTableComponent,
-    DeleteTableComponent,
-    EditTableComponent,
-    ChangeTableComponent,
-    ItemComponent,
-    AddItemComponent,
-    EditItemComponent,
-    DeleteItemComponent,
-    AddCategoryComponent,
-    EditCategoriesComponent,
-    AssignCategoryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +18,8 @@ import { AdminPanelComponent } from "./admin-panel/admin-panel.component";
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AdminPanelModule
   ],
   providers: [],
   bootstrap: [AppComponent],
